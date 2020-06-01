98545bb8af6473db916aee8b15af8b57fad350d0
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-01 15:28:18 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,6 +3,14 @@
 #   "Sarah" => 6,
 #   "Amanda" => 27
 # }
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
+
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
@@ -10,12 +18,5 @@ def age_appropriate_birthday(birthday_kids)
     else
       puts "You are to old for this."
     end
-  end
-end
-
-
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
